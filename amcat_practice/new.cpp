aa500a3823c0e1b385a8bb007182a948a0f09f47
Vinayak Arora <vinayakarora7461@gmail.com>
2025-04-14 19:47:36 +0530

    Implement code changes to enhance functionality and improve performance

---
+++ amcat_practice/new.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    vector<int> b(m);
+    for (int i = 0; i < m; i++)
+    {
+        cin >> b[i];
+    }
+    int ans = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (a[i] == b[j])
+            {
+                ans++;
+            }
+        }
+    }
+    cout << ans << endl;
+    return 0;
+}
